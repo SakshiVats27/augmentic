cfb6b4dc7ac84b6eb1819cf572065503baf94e65
SakshiVats27 <116297980+SakshiVats27@users.noreply.github.com>
2023-07-30 21:22:11 +0530

    nav.tsx

---
+++ nav.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Nav: React.FC = () => {
+  return (
+    <nav className="nav-v2-main" role="navigation" data-nav-v2-mobile>
+      <div className="nav-v2-main__wrapper">
+        <ul className="nav-v2-main__list">
+          <li className="nav-v2-main__item">
+            <a data-nav-event-clicked="Hire Designers" href="/hiring">Find talent</a>
+          </li>
+
+          <li className="nav-v2-main__item">
+            <a href="/for-designers" data-nav-event-clicked="For Designers">For designers</a>
+          </li>
+
+          <li className="nav-v2-main__item">
+            <a data-nav-event-clicked="Inspiration" href="/shots/popular">Inspiration</a>
+          </li>
+
+          <li className="nav-v2-main__item">
+            <a data-nav-event-clicked="Learn Design" href="/learn">Learn design</a>
+          </li>
+
+          <li className="nav-v2-main__item">
+            <a data-nav-event-clicked="Go Pro" href="/pro">Go Pro</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
